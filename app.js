baac0c7bb0c0848a8f6e01f1bcb7f0f48c997a90
satheshkumar-techversant <sathesh@techversantinfo.com>
2021-06-16 02:11:42 +0530

    modify app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function(req, res) {
-    res.write('under contruction!');
+    res.write('under contruction !');
     res.end();
 }).listen(8080, '0.0.0.0');
